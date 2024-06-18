e1793816428444b1cd34708f75904fa2c8871401
Aryanmittal23 <mittalaryan932@gmail.com>
2024-06-18 09:17:17 +0530

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import  { React, useState } from 'react';
 import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import { Routes,Route} from 'react-router-dom';
+import Cart from "./pages/Cart/Cart.tsx";
+import Home from './pages/Home/Home.tsx';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 
 function App() {
-  
+  const [showLogin,setShowLogin]=useState(false)
 
   return (
-    <div>
-      me
+  <>
+  {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+    <div className="app">
+      <Navbar setShowLogin={setShowLogin} />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
+    <Footer/>
+  </>
   )
 }
 
