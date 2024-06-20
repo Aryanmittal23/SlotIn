9c3ed0f6a9968d427bf679d519b3a82645046966
Aryanmittal23 <mittalaryan932@gmail.com>
2024-06-20 10:12:36 +0530

    add assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,21 @@
 import basket_icon from './basket_icon.png'
 import logo from './logo.png'
-//import header_img from './header_img.png'
+import header_img from './header_img.png'
+import header_img1 from './header_img1.png'
+
 import search_icon from './search_icon.png'
 
 //import add_icon_white from './add_icon_white.png'
 //import add_icon_green from './add_icon_green.png'
 //import remove_icon_red from './remove_icon_red.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+
+
 import app_store from './app_store.png'
 import play_store from './play_store.png'
 import linkedin_icon from './linkedin_icon.png'
@@ -21,6 +31,8 @@ import logout_icon from './logout_icon.png'
 
 export const assets = {
     logo,
+    header_img,
+    header_img1,
     basket_icon,
     search_icon,
     rating_starts,
@@ -35,4 +47,29 @@ export const assets = {
     logout_icon,
     bag_icon
 }
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Deserts",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Pure Veg",
+        menu_image: menu_6
+    }]
 export default assets
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './Header.css'
+
 function Header() {
   return (
     
       <div className='header'>
       <div className="header-contents">
-        <h2>Order your favourite food here</h2>
-        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest</p>
-        <button>View Menu</button>
+        <h2>
+          Say goodbye to long queues 
+      <br />
+      and wasted time with Slot-in</h2>
+        <p>the ultimate solution for busy individuals who value their time.</p>
+        
+        <button>Get Started</button>
+        
       </div>
     </div>
    
