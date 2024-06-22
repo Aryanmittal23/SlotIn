7fbc3cb217001ccd6e81f135b687ddd505b70e44
Aryanmittal23 <mittalaryan932@gmail.com>
2024-06-22 07:21:32 +0530

    add assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,12 +2,19 @@ import basket_icon from './basket_icon.png'
 import logo from './logo.png'
 import header_img from './header_img.png'
 import header_img1 from './header_img1.png'
+import header_img2 from './header_img2.png'
 
 import search_icon from './search_icon.png'
 
-//import add_icon_white from './add_icon_white.png'
-//import add_icon_green from './add_icon_green.png'
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
 //import remove_icon_red from './remove_icon_red.png'
+
+import about1 from './about1.png'
+import about2 from './about2.png'
+import about3 from './about3.png'
+
+
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
 import menu_3 from './menu_3.png'
@@ -15,7 +22,40 @@ import menu_4 from './menu_4.png'
 import menu_5 from './menu_5.png'
 import menu_6 from './menu_6.png'
 
+import food1 from './food1.png'
+import food2 from './food2.png'
+import food3 from './food3.png'
+import food4 from './food4.png'
+import food5 from './food5.png'
+import food6 from './food6.png'
+import food7 from './food7.png'
+import food8 from './food8.png'
+import food9 from './food9.png'
+import food10 from './food10.png'
+import food11 from './food11.png'
+import food12 from './food12.png'
+import food13 from './food13.png'
+import food14 from './food14.png'
+import food15 from './food15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
 
+import location2 from './location2.png'
 import app_store from './app_store.png'
 import play_store from './play_store.png'
 import linkedin_icon from './linkedin_icon.png'
@@ -30,11 +70,18 @@ import logout_icon from './logout_icon.png'
 //import parcel_icon from './parcel_icon.png'
 
 export const assets = {
+    about1,
+    about2,
+    about3,
+    location2,
     logo,
     header_img,
     header_img1,
+    header_img2,
     basket_icon,
     search_icon,
+    add_icon_green,
+    add_icon_white,
     rating_starts,
     app_store,
     play_store,
@@ -49,27 +96,250 @@ export const assets = {
 }
 export const menu_list = [
     {
-        menu_name: "Salad",
+        menu_name: "Hair Salon",
         menu_image: menu_1
     },
     {
-        menu_name: "Rolls",
+        menu_name: "Barbershop",
         menu_image: menu_2
     },
     {
-        menu_name: "Deserts",
+        menu_name: "Skin Care",
         menu_image: menu_3
     },
     {
-        menu_name: "Sandwich",
+        menu_name: "Nail Salon",
         menu_image: menu_4
     },
     {
-        menu_name: "Cake",
+        menu_name: "Massage",
         menu_image: menu_5
     },
     {
-        menu_name: "Pure Veg",
+        menu_name: "Makeup",
         menu_image: menu_6
-    }]
+    },
+    {
+        menu_name: "Barbershop",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Skin Care",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Nail Salon",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Massage",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Makeup",
+        menu_image: menu_6
+    }
+]
+
+    export const item_list = [
+        {
+            _id: "1",
+            name: " Hair Salon",
+            image: food1,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Hair Salon"
+        },
+        {
+            _id: "2",
+            name: " Hair Salon",
+            image: food2,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Hair Salon"
+        }, {
+            _id: "3",
+            name: " Hair Salon",
+            image: food3,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Hair Salon"
+        }, {
+            _id: "4",
+            name: " Hair Salon",
+            image: food4,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Hair Salon"
+        }, {
+            _id: "5",
+            name: " Nail Salon",
+            image: food5,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Nail Salon"
+        }, {
+            _id: "6",
+            name: " Nail Salon",
+            image: food6,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Nail Salon"
+        }, {
+            _id: "7",
+            name: " Nail Salon",
+            image: food7,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Nail Salon"
+        }, {
+            _id: "8",
+            name: " Nail Salon",
+            image: food8,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Nail Salon"
+        }, {
+            _id: "9",
+            name: "Cream",
+            image: food9,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Skin Care"
+        }, {
+            _id: "10",
+            name: " Ice Cream",
+            image: food10,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Skin Care"
+        }, {
+            _id: "11",
+            name: " Cream",
+            image: food11,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Skin Care"
+        }, {
+            _id: "12",
+            name: "Vanilla Cream",
+            image: food12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Skin Care"
+        },
+        {
+            _id: "13",
+            name: " Makeup",
+            image: food13,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Makeup"
+        },
+        {
+            _id: "14",
+            name: "Vegan Makeup",
+            image: food14,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Makeup"
+        }, {
+            _id: "15",
+            name: " Makeup",
+            image: food15,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Makeup"
+        }, {
+            _id: "16",
+            name: " Makeup",
+            image: food_16,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Makeup"
+        }, {
+            _id: "17",
+            name: "Cup Barbershop",
+            image: food_17,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Barbershop"
+        }, {
+            _id: "18",
+            name: "Vegan Barbershop",
+            image: food_18,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Barbershop"
+        }, {
+            _id: "19",
+            name: "Barbershop",
+            image: food_19,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Barbershop"
+        }, {
+            _id: "20",
+            name: "Barbershop",
+            image: food_20,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Barbershop"
+        }, {
+            _id: "21",
+            name: "Garlic Mushroom ",
+            image: food_21,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pure Veg"
+        }, {
+            _id: "22",
+            name: "Fried Cauliflower",
+            image: food_22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pure Veg"
+        }, {
+            _id: "23",
+            name: "Mix Veg Pulao",
+            image: food_23,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pure Veg"
+        }, {
+            _id: "24",
+            name: "Rice Zucchini",
+            image: food_24,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pure Veg"
+        },
+        {
+            _id: "25",
+            name: "Cheese Pasta",
+            image: food_25,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pasta"
+        },
+        {
+            _id: "26",
+            name: "Tomato Pasta",
+            image: food_26,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pasta"
+        }, {
+            _id: "27",
+            name: "Creamy Pasta",
+            image: food_27,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pasta"
+        }, {
+            _id: "28",
+            name: "Chicken Pasta",
+            image: food_28,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pasta"
+        }, {
+            _id: "29",
+            name: "Buttter Noodles",
+            image: food_29,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Noodles"
+        }, {
+            _id: "30",
+            name: "Veg Noodles",
+            image: food_30,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Noodles"
+        }, {
+            _id: "31",
+            name: "Somen Noodles",
+            image: food_31,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Noodles"
+        }, {
+            _id: "32",
+            name: "Cooked Noodles",
+            image: food_32,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Noodles"
+        }
+    ]
+    
 export default assets
