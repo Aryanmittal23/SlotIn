95b0ef64cd50e7534e13167eeb688ccda01c69bc
Aryanmittal23 <mittalaryan932@gmail.com>
2024-06-28 18:01:23 +0530

    images

---
+++ frontend/src/images/images.js
@@ -0,0 +1,19 @@
+import playstore from './playstore.png'
+import customer_management from './customer_management.png'
+import list_service from './list_service.png'
+import manage_appointment from './manage_appointment.png'
+import manage_booking from './manage_booking.png'
+import organize_services from './organize_services.png'
+import register from './register.png'
+
+
+export const images = {
+    playstore,
+    customer_management,
+    list_service,
+    manage_appointment,
+    manage_booking,
+    organize_services,
+    register
+}
+export default images;
