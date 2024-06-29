73e4bee91da8cbb462fad87d84b67fe7af9b3c15
Aryanmittal23 <mittalaryan932@gmail.com>
2024-06-30 02:31:51 +0530

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,12 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { StoreContextProvider } from './context/StoreContext.tsx'
-import { MantineProvider } from '@mantine/core';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
   <StoreContextProvider>
-  <MantineProvider>
   <App />
-  </MantineProvider>
-    
   </StoreContextProvider>
   </BrowserRouter>
 )
